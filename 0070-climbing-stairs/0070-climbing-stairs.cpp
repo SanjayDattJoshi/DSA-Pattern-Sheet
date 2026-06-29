393e24c23d1425cf75388666fb0c57c48565d033
Sanjay Datt Joshi <sanjayjoshi7409882635@gmail.com>
2026-06-29 10:23:34 +0530

    Time: 0 ms (100%), Space: 8.5 MB (50.55%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,13 +1,5 @@
 class Solution {
 public:
-int fun(int i, int n,vector<int> &dp){
-    if(i>n) return 0;
-    if(i==n) return 1;
-    if(dp[i] != -1) return dp[i];
-    int ways = fun(i+1, n, dp) + fun(i+2, n, dp);
-    return dp[i] = ways;
-}
-public:
     int climbStairs(int n) {
         vector<int> dp(n+2,0);
         dp[n] = 1;
