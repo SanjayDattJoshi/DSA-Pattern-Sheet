9407d019ed5cd5c508cc0b10a2b82c618b6e26a1
Sanjay Datt Joshi <sanjayjoshi7409882635@gmail.com>
2026-06-29 10:23:14 +0530

    Time: 0 ms (100%), Space: 8.7 MB (18.51%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -9,7 +9,12 @@ int fun(int i, int n,vector<int> &dp){
 }
 public:
     int climbStairs(int n) {
-        vector<int> dp(n,-1);
-        return fun(0, n,dp);
+        vector<int> dp(n+2,0);
+        dp[n] = 1;
+        for(int i=n-1; i>=0; i--){
+            int ways = dp[i+1] + dp[i+2];
+            dp[i] = ways;
+        }
+        return dp[0];
     }
 };
