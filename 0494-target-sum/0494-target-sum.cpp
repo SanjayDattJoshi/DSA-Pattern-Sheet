1d5c5bb1aa3180716c9d8be8cc1bb4ec7f985848
Sanjay Datt Joshi <sanjayjoshi7409882635@gmail.com>
2026-06-29 14:48:22 +0530

    Time: 3 ms (85.63%), Space: 15.3 MB (52.95%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+int fun(vector<int>& arr, int ind, int sum, vector<vector<int>>& dp) {
+
+        if(ind == 0){
+        if(sum == 0 && arr[0] == 0) return 2;
+        if(sum == 0 || sum == arr[0]) return 1;
+        return 0;
+        
+        if (sum == 0) return 1;
+
+        }
+        if (dp[ind][sum] != -1)
+            return dp[ind][sum];
+
+        int notTake = fun(arr, ind - 1, sum, dp);
+
+        int take = 0;
+        if (arr[ind] <= sum)
+            take = fun(arr, ind - 1, sum - arr[ind], dp);
+
+        return dp[ind][sum] = (take + notTake);
+}
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int totalSum = 0;
+        for(auto it: nums) totalSum += it;
+        if (totalSum + target < 0 || (totalSum + target) % 2 != 0) return 0;
+        int pos = (totalSum + target)/2;
+
+        if(totalSum<abs(target)) return 0;
+        if((totalSum+target)%2) return 0;
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(pos+1, -1));
+        int res = 0;
+        res = fun(nums, n-1,pos,dp);
+        return res;
+    }
+};
