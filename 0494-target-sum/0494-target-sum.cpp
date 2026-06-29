dcf2cb99848bc86b2ee7ce494d523adf44884817
Sanjay Datt Joshi <sanjayjoshi7409882635@gmail.com>
2026-06-29 14:54:13 +0530

    Time: 5 ms (71.3%), Space: 15.4 MB (52.95%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -2,23 +2,20 @@ class Solution {
 public:
 int fun(vector<int>& arr, int ind, int sum, vector<vector<int>>& dp) {
 
-        if(ind == 0){
-        if(sum == 0 && arr[0] == 0) return 2;
-        if(sum == 0 || sum == arr[0]) return 1;
-        return 0;
-        
+    if(ind == 0){
+    if(sum == 0 && arr[0] == 0) return 2;
+    if(sum == 0 || sum == arr[0]) return 1;
+    return 0;
 
-        }
-        if (dp[ind][sum] != -1)
-            return dp[ind][sum];
+    }
+    if (dp[ind][sum] != -1) return dp[ind][sum];
 
-        int notTake = fun(arr, ind - 1, sum, dp);
+    int notTake = fun(arr, ind - 1, sum, dp);
 
-        int take = 0;
-        if (arr[ind] <= sum)
-            take = fun(arr, ind - 1, sum - arr[ind], dp);
+    int take = 0;
+    if (arr[ind] <= sum) take = fun(arr, ind - 1, sum - arr[ind],dp);
 
-        return dp[ind][sum] = (take + notTake);
+    return dp[ind][sum] = (take + notTake);
 }
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
@@ -26,7 +23,6 @@ public:
         for(auto it: nums) totalSum += it;
         if(totalSum < abs(target) || (totalSum + target) % 2 != 0)
         return 0;
-
         int pos = (totalSum + target) / 2;
         int n = nums.size();
         vector<vector<int>> dp(n, vector<int>(pos+1, -1));
