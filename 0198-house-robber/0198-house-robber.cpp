979ff9737cbfa4e66c2378d979c4bfc68369680c
Sanjay Datt Joshi <sanjayjoshi7409882635@gmail.com>
2026-06-29 10:51:38 +0530

    Time: 0 ms (100%), Space: 10.7 MB (45.22%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -11,18 +11,27 @@ class Solution {
 
 
 //Memoization
-int fun(vector<int> &dp,int i, int n, vector<int> &arr){
-    if(i>n) return 0;
-    if(i==n) return arr[n];
-    if(dp[i] != -1) return dp[i];
-    int rob = arr[i] + fun(dp,i+2, n, arr);
-    int skip = fun(dp,i+1, n, arr);
-    return dp[i] = max(rob,skip);
-}
+// int fun(vector<int> &dp,int i, int n, vector<int> &arr){
+//     if(i>n) return 0;
+//     if(i==n) return arr[n];
+//     if(dp[i] != -1) return dp[i];
+//     int rob = arr[i] + fun(dp,i+2, n, arr);
+//     int skip = fun(dp,i+1, n, arr);
+//     return dp[i] = max(rob,skip);
+// }
+
 
 public:
     int rob(vector<int>& nums) {
-        vector<int> dp(nums.size(), -1);
-        return fun(dp,0,nums.size()-1, nums);
+        int n = nums.size();
+        vector<int> dp(n+2, 0);
+        dp[n-1] = nums[n-1];
+        dp[n] = 0;
+        for(int i=n-1; i>=0; i--){
+            int rob = nums[i] + dp[i+2];
+            int skip = dp[i+1];
+            dp[i] = max(rob,skip);
+        }
+        return dp[0];
     }
 };
