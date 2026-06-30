8907cd89c6631414276b3ed9cf249254e5fc005d
Sanjay Datt Joshi <sanjayjoshi7409882635@gmail.com>
2026-06-30 14:02:03 +0530

    Time: 2 ms (13.3%), Space: 12 MB (6.23%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -11,7 +11,21 @@ int fun(vector<vector<int>> &grid,vector<vector<int>>&dp, int i, int j){
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
         int n = grid.size(), m = grid[0].size();
-        vector<vector<int>> dp(n, vector<int>(m,-1));
-        return fun(grid,dp, 0, 0);
+        vector<vector<long long>> dp(n+1, vector<long long>(m+1,0));
+        dp[n-1][m-1] = 1;
+        for(int i=n-1; i>=0; i--){
+            for(int j=m-1; j>=0; j--){
+                if(grid[i][j] == 1) {
+                    dp[i][j] = 0;
+                }
+                else if(i == n - 1 && j == m - 1) {
+                    dp[i][j] = 1;
+                }
+                else {
+                    dp[i][j] = dp[i][j + 1] + dp[i + 1][j];
+                }
+            }
+        }
+        return dp[0][0];
     }
 };
