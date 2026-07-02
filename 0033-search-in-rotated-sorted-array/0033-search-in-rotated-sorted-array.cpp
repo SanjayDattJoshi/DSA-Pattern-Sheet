bd761aa9d055c2544a701bba6b6628b9c0c85289
Sanjay Datt Joshi <sanjayjoshi7409882635@gmail.com>
2026-07-02 14:25:14 +0530

    Time: 0 ms (100%), Space: 15.3 MB (34.52%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,34 +1,27 @@
 class Solution {
-int bs(vector<int> &arr, int start, int end, int t){
-    int res=INT_MAX;
-    int n = arr.size();
-    int i=start, j=end;
-    while(i<=j){
-        int m = i + (j-i)/2;
-        if(arr[m]==t) return m;
-        else if(arr[m]>t) j = m-1;
-        else i = m + 1;
-    }
-    return res;
-}
 public:
     int search(vector<int>& nums, int target) {
         int n = nums.size();
         int low=0,high=n-1,res=-1;
-        int part = 0;
-        while(low<high){
+        while(low<=high){
             int mid = low + (high-low)/2;
-            if(nums[mid]>nums[high]){
-                low = mid+1;
+            if(nums[mid]==target) return mid;
+            
+            // Left half is sorted
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target < nums[mid])
+                    high = mid - 1;
+                else
+                    low = mid + 1;
             }
-            else{
-                high=mid;
+            // Right half is sorted
+            else {
+                if (nums[mid] < target && target <= nums[high])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
             }
         }
-        part = low;
-        int val1 = bs(nums, 0, part-1, target);
-        int val2 = bs(nums, part, n-1, target);
-        res = val1==INT_MAX?val2:val1;
-        return res==INT_MAX?-1:res;
+        return res;
     }
 };
