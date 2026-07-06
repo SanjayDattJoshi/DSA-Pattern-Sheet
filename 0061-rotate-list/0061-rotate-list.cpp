47e3b4b7d00cc56e1127e01c591133cf10a31c4f
Sanjay Datt Joshi <sanjayjoshi7409882635@gmail.com>
2026-07-06 15:35:50 +0530

    Time: 0 ms (100%), Space: 16.3 MB (64.25%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -11,27 +11,34 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        if(head==nullptr || k==0) return head;
+        if (head == nullptr || k == 0)
+            return head;
         int cnt = 0;
-        ListNode* temp = head, *newHead=nullptr;
-        while(temp){
+        ListNode *temp = head, *newHead = nullptr;
+        while (temp) {
             cnt++;
-            temp=temp->next;
+            temp = temp->next;
         }
 
-        k = k%cnt;
-        if(k==0) return head;
-            int pos = cnt-k-1;
-            ListNode* t = head, *nextNode;
-            while(t && pos--){
-                t = t->next;
-            }
-            nextNode = t->next;
-            t->next = nullptr;
-            newHead = nextNode;
-            while(nextNode && nextNode->next) nextNode=nextNode->next;
-            if(nextNode) nextNode->next = head;
-            return newHead;
-        
+        k = k % cnt;
+        if (k == 0) return head;
+
+        int pos = cnt - k - 1;
+        ListNode *t = head, *nextNode;
+        while (t && pos--) {
+            t = t->next;
+        }
+
+        nextNode = t->next;
+        t->next = nullptr;
+        newHead = nextNode;
+
+        while (nextNode && nextNode->next)
+            nextNode = nextNode->next;
+
+        if (nextNode)
+            nextNode->next = head;
+            
+        return newHead;
     }
 };
