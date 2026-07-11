57b13017a387be4c058786e5750b6ef08056101b
Sanjay Datt Joshi <sanjayjoshi7409882635@gmail.com>
2026-07-11 15:04:07 +0530

    Time: 3 ms (53.45%), Space: 12.1 MB (11.65%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        int freq[26] = {0};
-
-        for (char c : magazine)
-            freq[c - 'a']++;
-
-        for (char c : ransomNote) {
-            if (freq[c - 'a'] == 0)
-                return false;
-
-            freq[c - 'a']--;
+        int n = ransomNote.length(), m = magazine.length();
+        unordered_map<char, int> mpp1, mpp2;
+        for(int i=0; i<n; i++){
+            mpp1[ransomNote[i]]++;
         }
-
+        for(int i=0; i<m; i++){
+            mpp2[magazine[i]]++;
+        }
+        for(auto it: mpp1){
+            if(mpp2.find(it.first) == mpp2.end() || it.second > mpp2[it.first]) return false;
+        }
         return true;
     }
 };
