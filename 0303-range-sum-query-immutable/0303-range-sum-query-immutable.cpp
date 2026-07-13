9fb0aee132e164b06ba5f968dbdb56ba68285c65
Sanjay Datt Joshi <sanjayjoshi7409882635@gmail.com>
2026-07-13 13:54:14 +0530

    Time: 0 ms (100%), Space: 24 MB (62.83%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
@@ -1,35 +1,25 @@
 class NumArray {
-    vector<int> seg;
+    vector<int> prefixSum;
     int n;
-    void build(vector<int> &nums, int node, int low, int high){
-            if(low==high) {
-                seg[node] = nums[low];
-                return;
-            }
-            int mid = (low+high)/2;
-            build(nums,2*node, low, mid);
-            build(nums,2*node+1, mid+1, high);
-            seg[node] = seg[2*node] + seg[2*node+1];
-            return;
-    }
-    int query(int node, int start, int end, int l, int r){
-            if(end < l || start > r) return 0;
-            if(start >= l && end <= r) return seg[node];
-            int mid = (start+end)/2;
-            int left = query(2*node, start, mid, l, r);
-            int right = query(2*node+1, mid+1, end, l, r);
-            return left + right;
+    void preSum(vector<int> &nums){
+        prefixSum[0] = nums[0];
+        for(int i=1; i<n; i++){
+            prefixSum[i] = prefixSum[i-1]+nums[i];
         }
+    }
+    int preResult(int left, int right){
+        if(left == 0) return prefixSum[right];
+        return prefixSum[right] - prefixSum[left-1];
+    }
 public:
     NumArray(vector<int>& nums) {
         n = nums.size();
-        seg.resize(4 * n);
-        if(n>0) build(nums,1,0,n-1);
-        
-    };
+        prefixSum.resize(n);
+        preSum(nums);
+    }
     
     int sumRange(int left, int right) {
-        return query(1, 0, n-1, left, right);
+        return preResult(left, right);
     }
 };
 
