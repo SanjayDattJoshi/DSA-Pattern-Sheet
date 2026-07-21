2a73267ce08b7b23154e9e79ed75c23ffb357398
Sanjay Datt Joshi <sanjayjoshi7409882635@gmail.com>
2026-07-21 13:43:51 +0530

    Time: 23 ms (54.14%), Space: 17.5 MB (55.62%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -2,24 +2,32 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         int n = nums.size();
-        set<vector<int>> st;
-        for(int a=0; a<n; a++){
-            for(int b = a+1; b<n; b++){
-                set<long long> hashSet;
-                for(int c = b+1; c<n; c++){
-                    long long sum = nums[a] + nums[b];
-                    sum += nums[c];
-                    long long d = target - sum;
-                    if(hashSet.find(d) != hashSet.end()){
-                        vector<int> temp = {nums[a], nums[b], nums[c], (int)d};
-                        sort(temp.begin(), temp.end());
-                        st.insert(temp);
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        for(int i=0; i<n; i++){
+            if(i>0 && nums[i] == nums[i-1]) continue;
+            for(int j=i+1; j<n; j++){
+                if(j>i+1 && nums[j] == nums[j-1]) continue;
+                int k = j+1, l = n-1;
+                while(k<l){
+                    long long sum = nums[i];
+                    sum += nums[j];
+                    sum += nums[k];
+                    sum += nums[l];
+                    if(sum==target) {
+                        ans.push_back({nums[i], nums[j], nums[k], nums[l]});
+                        k++;
+                        l--;
+                        while(k<l && nums[k] == nums[k-1]) k++;
+                        while(k<l && nums[l] == nums[l+1]) l--;
                     }
-                    hashSet.insert(nums[c]);
+                    else if(sum<target){
+                        k++;
+                    }
+                    else l--;
                 }
             }
         }
-        vector<vector<int>> ans(st.begin(), st.end());
         return ans;
     }
 };
