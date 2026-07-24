5e651ba6295fe3dce697317a469e1cb8bd035a9e
Sanjay Datt Joshi <sanjayjoshi7409882635@gmail.com>
2026-07-24 11:47:26 +0530

    Time: 7 ms (20.64%), Space: 16.7 MB (10.91%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     string frequencySort(string s) {
@@ -16,6 +17,30 @@ public:
             pq.pop();
             while (f--) {
             res += ch;
+            }
+        }
+        return res;
+    }
+};*/
+class Solution {
+public:
+    string frequencySort(string s) {
+        int n = s.length();
+        unordered_map<char, int> freq;
+        for(int i=0; i<n; i++){
+            freq[s[i]]++;
+        }
+        string res = "";
+        vector<vector<char>> bucket(n + 1);
+        for(auto it: freq){
+            bucket[it.second].push_back(it.first);
+        }
+        for(int i=n; i>0; i--){
+            for (char ch : bucket[i]) {
+                int fre = i;
+                while (fre--) {
+                res += ch;
+                }
             }
         }
         return res;
