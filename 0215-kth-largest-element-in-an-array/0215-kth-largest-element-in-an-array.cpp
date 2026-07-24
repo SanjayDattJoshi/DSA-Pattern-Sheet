f33e465576a5e0f1ea4811a472eff96606ef37c7
Sanjay Datt Joshi <sanjayjoshi7409882635@gmail.com>
2026-07-24 12:09:03 +0530

    Time: 25 ms (80.28%), Space: 72.5 MB (50.01%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -7,9 +7,10 @@ public:
         for(i=0; i<k; i++) pq.push(nums[i]);
 
         for(i=k; i<n; i++){
-            if(nums[i]<pq.top()) continue;
-            pq.pop();
-            pq.push(nums[i]);
+            if (nums[i] > pq.top()) {
+                pq.pop();
+                pq.push(nums[i]);
+            }
         }
         return pq.top();
     }
