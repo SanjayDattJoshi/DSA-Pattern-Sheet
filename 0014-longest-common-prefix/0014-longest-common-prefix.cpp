21916cea11ad99ad20923e19eaea4a10c90b97cf
Sanjay Datt Joshi <sanjayjoshi7409882635@gmail.com>
2026-08-10 21:03:44 +0530

    Time: 0 ms (100%), Space: 12 MB (25.67%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -4,16 +4,19 @@ public:
         if (strs.empty())
             return "";
 
-        for (int i = 0; i < strs[0].size(); i++) {
-            char ch = strs[0][i];
+        sort(strs.begin(), strs.end());
 
-            for (int j = 1; j < strs.size(); j++) {
-                if (i >= strs[j].size() || strs[j][i] != ch) {
-                    return strs[0].substr(0, i);
-                }
-            }
+        string first = strs[0];
+        string last = strs.back();
+
+        int i = 0;
+
+        while (i < first.size() &&
+               i < last.size() &&
+               first[i] == last[i]) {
+            i++;
         }
 
-        return strs[0];
+        return first.substr(0, i);
     }
 };
