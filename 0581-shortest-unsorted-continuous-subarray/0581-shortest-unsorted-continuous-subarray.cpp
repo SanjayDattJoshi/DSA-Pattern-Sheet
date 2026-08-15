9f4ba7d73aeb7eb15dda875f355d20c9b566d26c
Sanjay Datt Joshi <sanjayjoshi7409882635@gmail.com>
2026-08-15 20:55:34 +0530

    Time: 0 ms (100%), Space: 30.5 MB (52.16%) - LeetHub

--- 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.cpp
+++ 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.cpp
@@ -1,25 +1,40 @@
 class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
-        vector<int> sorted = nums;
-
-        sort(sorted.begin(), sorted.end());
-
         int n = nums.size();
 
         int left = 0;
+        int right = n - 1;
 
-        while(left < n && nums[left] == sorted[left]) {
+        // Find first position where order breaks from left
+        while(left < n - 1 && nums[left] <= nums[left + 1]) {
             left++;
         }
 
-        if(left == n)
+        // Already sorted
+        if(left == n - 1)
             return 0;
 
-        int right = n - 1;
-
-        while(right >= 0 && nums[right] == sorted[right]) {
+        // Find first position where order breaks from right
+        while(right > 0 && nums[right - 1] <= nums[right]) {
             right--;
+        }
+
+        // Find min and max in the unsorted portion
+        int mn = INT_MAX;
+        int mx = INT_MIN;
+
+        for(int i = left; i <= right; i++) {
+            mn = min(mn, nums[i]);
+            mx = max(mx, nums[i]);
+        }
+
+        while(left > 0 && nums[left - 1] > mn) {
+            left--;
+        }
+
+        while(right < n - 1 && nums[right + 1] < mx) {
+            right++;
         }
 
         return right - left + 1;
