a0c768b8615e01675220651289c240d24c07aa28
Sanjay Datt Joshi <sanjayjoshi7409882635@gmail.com>
2026-08-20 13:33:34 +0530

    Time: 0 ms (100%), Space: 23 MB (55.77%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -11,37 +11,17 @@
  */
 class Solution {
 public:
-    int height(TreeNode* root){
-        if(root==nullptr) return 0;
-        queue<TreeNode*> q;
-        q.push(root);
-        int h = 0;
-        while(!q.empty()){
-            
-            int size = q.size();
-            for(int i=0; i<size; i++){
-                TreeNode* node = q.front();
-                q.pop();
-                if(node->left) q.push(node->left);
-                if(node->right) q.push(node->right);
-            }
-            h++;
-        }
-        return h;
+    int dfs(TreeNode* root){
+            if(root==nullptr) return 0;
+            int left = dfs(root->left);
+            if(left==-1) return -1;
+            int right = dfs(root->right);
+            if(right==-1) return -1;
+            if(abs(right-left) > 1) return -1;
+            return max(left, right) + 1;
     }
     bool isBalanced(TreeNode* root) {
         if(root == nullptr) return true;
-        queue<TreeNode*> q;
-        q.push(root);
-        while(!q.empty()){
-            TreeNode* node = q.front();
-            q.pop();
-            int left = height(node->left);
-            int right = height(node->right);
-            if(abs(right-left)>1) return false;
-            if(node->left) q.push(node->left);
-            if(node->right) q.push(node->right);
-        }
-        return true;
+        return dfs(root)!=-1;
     }
 };
