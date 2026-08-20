9bb9f268d8a0ceaed85999d730e9a31d656db5ab
Sanjay Datt Joshi <sanjayjoshi7409882635@gmail.com>
2026-08-20 10:59:40 +0530

    Time: 0 ms (100%), Space: 18.5 MB (40.87%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -9,20 +9,36 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+//  class Solution {
+// private:
+// bool isMirrorTree(TreeNode* p, TreeNode* q) {
+//         if(p==nullptr && q==nullptr) return true;
+//         if(p==nullptr || q==nullptr) return false;
+//         if(p->val != q->val) return false;
+//         bool l = isMirrorTree(p->left, q->right);
+//         bool r = isMirrorTree(p->right, q->left);
+//         if(l==true && r==true) return true;
+//         return false;
+//     }
+// public:
+//     bool isSymmetric(TreeNode*root) {
+//         if(root == nullptr) return true;
+//         return isMirrorTree(root->left, root->right); 
+//     }
+// };
 class Solution {
-private:
-bool isMirrorTree(TreeNode* p, TreeNode* q) {
-        if(p==nullptr && q==nullptr) return true;
-        if(p==nullptr || q==nullptr) return false;
+public:
+    bool compare(TreeNode* p, TreeNode* q){
+        if(p == nullptr && q == nullptr) return true;
+        if(p == nullptr || q == nullptr) return false;
         if(p->val != q->val) return false;
-        bool l = isMirrorTree(p->left, q->right);
-        bool r = isMirrorTree(p->right, q->left);
-        if(l==true && r==true) return true;
+        bool left = true, right = true;
+        left = compare(p->left, q->right);
+        right = compare(p->right, q->left);
+        if(left && right) return true;
         return false;
     }
-public:
-    bool isSymmetric(TreeNode*root) {
-        if(root == nullptr) return true;
-        return isMirrorTree(root->left, root->right); 
+    bool isSymmetric(TreeNode* root) {
+        return compare(root->left, root->right);
     }
 };
