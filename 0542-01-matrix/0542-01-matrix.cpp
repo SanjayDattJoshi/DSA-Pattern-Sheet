bd8971fe58135ccb696472614d242d746bdba9a7
Sanjay Datt Joshi <sanjayjoshi7409882635@gmail.com>
2026-09-03 10:34:25 +0530

    Time: 15 ms (71.56%), Space: 34.7 MB (76.28%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,49 +1,40 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-
         int n = mat.size();
         int m = mat[0].size();
 
         queue<pair<int,int>> q;
 
-        // Put all 0s into the queue
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
+        vector<vector<int>> res(n, vector<int> (m,-1));
 
-                if(mat[i][j] == 0) {
-                    q.push({i, j});
-                }
-                else {
-                    mat[i][j] = -1;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(mat[i][j]==0){
+                    q.push({i,j});
+                    res[i][j] = 0;
                 }
             }
         }
+        int dr[] = {-1,0,1,0};
+        int dc[] = {0,1,0,-1};
 
-        int dr[4] = {-1, 0, 1, 0};
-        int dc[4] = {0, 1, 0, -1};
-
-        while(!q.empty()) {
-
-            auto [row, col] = q.front();
+        while(!q.empty()){
+            auto p = q.front();
             q.pop();
+            int row = p.first;
+            int col = p.second;
 
-            for(int k = 0; k < 4; k++) {
+            for(int i=0; i<4; i++){
+                int newr = row + dr[i];
+                int newc = col + dc[i];
 
-                int newr = row + dr[k];
-                int newc = col + dc[k];
-
-                if(newr >= 0 && newr < n &&
-                   newc >= 0 && newc < m &&
-                   mat[newr][newc] == -1) {
-
-                    mat[newr][newc] = mat[row][col] + 1;
-
+                if((newr>=0 && newr<n && newc>=0 && newc<m) && res[newr][newc]== -1){
+                    res[newr][newc] = res[row][col] + 1;
                     q.push({newr, newc});
                 }
             }
         }
-
-        return mat;
+        return res;
     }
 };
