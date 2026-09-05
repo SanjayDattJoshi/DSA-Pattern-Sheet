6369dbc3ba94d00fc6d6509fe1fe6db04f3d8795
Sanjay Datt Joshi <sanjayjoshi7409882635@gmail.com>
2026-09-05 16:05:36 +0530

    Time: 0 ms (100%), Space: 8.2 MB (82.97%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -14,8 +14,14 @@ int totalPaths(vector<vector<int>> &arr, int i, int j, vector<vector<int>> &dp)
 }
 public:
     int uniquePaths(int m, int n) {
-    vector<vector<int>> arr(n, vector<int>(m));
-    vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
-    return totalPaths(arr, n - 1, m - 1,dp);
+    vector<int> dp(n, 1);
+
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                dp[j] = dp[j] + dp[j-1];
+            }
+        }
+
+        return dp[n-1];
     }
 };
