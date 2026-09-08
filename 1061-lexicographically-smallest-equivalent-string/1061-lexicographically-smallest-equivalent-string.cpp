e07a18c49d78be97d6459732211e0dacbcab2219
Sanjay Datt Joshi <sanjayjoshi7409882635@gmail.com>
2026-09-08 16:22:02 +0530

    Time: 0 ms (100%), Space: 8.9 MB (74.94%) - LeetHub

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.cpp
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.cpp
@@ -1,3 +1,32 @@
+class Disjoint_Set {
+    vector<int> parent;
+
+public:
+    Disjoint_Set() {
+        parent.resize(26);
+        for (int i = 0; i < 26; i++)
+            parent[i] = i;
+    }
+
+    int findParent(int node) {
+        if (parent[node] == node)
+            return node;
+
+        return parent[node] = findParent(parent[node]);
+    }
+
+    void unite(char u, char v) {
+        int pu = findParent(u - 'a');
+        int pv = findParent(v - 'a');
+
+        if (pu == pv) return;
+
+        if (pu < pv)
+            parent[pv] = pu;
+        else
+            parent[pu] = pv;
+    }
+};
 class Solution {
 public:
     char dfs(unordered_map<char, vector<char>> &adj, char ch, vector<int> &visited){
@@ -15,6 +44,10 @@ public:
         int n = s1.length();
         int m = baseStr.length();
 
+
+        /*
+        //for DFS solution-->
+
         unordered_map<char, vector<char>> adj;
 
         for(int i=0; i<n; i++){
@@ -30,6 +63,17 @@ public:
             char minChar = dfs(adj, ch, visited);
 
             res.push_back(minChar);
+        }*/
+
+        Disjoint_Set dsu;
+        for(int i=0; i<n; i++){
+            dsu.unite(s1[i], s2[i]);
+        }
+
+        string res;
+
+        for(char ch : baseStr) {
+            res += dsu.findParent(ch-'a')  + 'a';
         }
         return res;
     }
