cbfc273b089b6abb85cf8625f12926062812f116
Sanjay Datt Joshi <sanjayjoshi7409882635@gmail.com>
2026-09-11 21:24:41 +0530

    Time: 0 ms (100%), Space: 9.9 MB (76.99%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        for(int i=0; i<s.length(); i++){
-            if((s[i] >= 'a' && s[i]<='z') || (s[i] >= '0' && s[i]<= '9')) continue;
-            else if(s[i]>='A' && s[i] <= 'Z') s[i] = tolower(s[i]);
-            else{
-                s.erase(i,1);
-                i--;
-            }
-        }
+        int i = 0;
+        int j = s.length() - 1;
 
-        int i = 0, j= s.length()-1;
-        while(i<j){
-            if(s[i]!=s[j]) return false;
+        while(i < j) {
+
+            while(i < j && !isalnum(s[i]))
+                i++;
+
+            while(i < j && !isalnum(s[j]))
+                j--;
+
+            if(tolower(s[i]) != tolower(s[j]))
+                return false;
+
             i++;
             j--;
         }
+
         return true;
     }
 };
