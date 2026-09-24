ae486f99d49ca620021dcd613b744b1fd6c7cab6
Sanjay Datt Joshi <sanjayjoshi7409882635@gmail.com>
2026-09-24 12:56:14 +0530

    Time: 0 ms (100%), Space: 9.1 MB (53.46%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -3,11 +3,9 @@ public:
     int myAtoi(string s) {
         int i = 0, n = s.size();
 
-        // Skip spaces
         while (i < n && s[i] == ' ')
             i++;
 
-        // Sign
         int sign = 1;
         if (i < n && (s[i] == '+' || s[i] == '-')) {
             if (s[i] == '-')
@@ -17,7 +15,6 @@ public:
 
         long long num = 0;
 
-        // Digits
         while (i < n && isdigit(s[i])) {
             num = num * 10 + (s[i] - '0');
 
